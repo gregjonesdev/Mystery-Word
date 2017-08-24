dea3b82a207208038bee841ed91d0162fc9fab8b
Greg Jones <gmjones@gmail.com>
2017-08-24 04:05:25 -0500

    change to npm word list

--- models/words.js
+++ models/words.js
@@ -1,6 +1,16 @@
 const fs = require('fs')
 
-const allWords = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n")
+let allWords = require('word-list-google')
+let words = []
+
+for(i=0; i<1000; i++) {
+  if (allWords.englishUsaNoSwears[i].length>4 && allWords.englishUsaNoSwears[i].length <10) {
+    words.push(allWords.englishUsaNoSwears[i])
+  }
+}
+
+console.log(words)
+//const allWords = fs.readFileSync(wordlist, "utf-8").toLowerCase().split("\n")
 let mysteryWord = function (i) {
   return allWords[i].split("")
 }
